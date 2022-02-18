e529fc9959e8ba3aeae6bdd8eef638ba086529cc
jieren <92771462+7ieren@users.noreply.github.com>
2022-02-18 19:44:39 +0800

    Updated index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-    loadExample(); // Load random sneaker into carousel featuring sneaker slide
-    loadLatest();
+    $(".latest").html(`<lottie-player id ="loading-latest" src="https://assets6.lottiefiles.com/packages/lf20_FT6Ci5.json"  background="transparent"  speed="1"  style="width: 500px; height: 500;"  loop  autoplay></lottie-player>`); // Clear the latest section
+    
+    loadExample(); 
+    loadLatest(); 
 });
 
 function loadExample() {
@@ -15,28 +17,13 @@ function loadExample() {
             <h5 style = "-webkit-text-stroke: 1px white; font-weight: normal; font-size: 30px; color: black;">${data.name}</h5>
             </div>
         </a>`
-        $(".featured-product").html(html);
+        $(".random-product").html(html);
     })
-
-    .catch(function(e) { // If there is an error getting the sneaker, use a place holder sneaker
-        console.log(e);
-        var html = (`
-            <div class="carousel-feature-slide" onclick=selectCard('c116b14f-8f00-454b-915c-f3f51c7a297c')>
-                <img src="https://image.uniqlo.com/UQ/ST3/sg/imagesgoods/445769/sub/sggoods_445769_sub2.jpg?width=1600&impolicy=quality_75" class="d-block w-100">
-                <div class="carousel-caption carousel-feature-text d-md-block">
-                    <h5>FEATURING</h5>
-                    <p>Wool blended oversized pea coat<p>
-                </div>
-            </div>
-        `);
-        $(".featured-product").html(html);
-    });
 }
 
 function loadLatest() {
-    $(".latest").html(""); // Clear the latest section
-    $(".latest").css("justify-content","flex-start"); // Set the latest section back to left aligned
-
+    $(".latest").html(""); 
+    $(".latest").css("justify-content","flex-start"); 
     fetch("https://example-data.draftbit.com/products?_limit=5") 
     .then(response => response.json())
     .then(function(data) {
@@ -46,9 +33,9 @@ function loadLatest() {
                 <div class="card" id="${s.id}" onclick="selectCard('${s.id}')">
                     <img src="${s.image_url}" style = "width:40%; height: 100%;">
                     <div class="card-body">
-                        <span class="product-brand">${s.brand}</span>
-                        <span class="product-name">${s.name}</span>
-                        <span class="product-price">$${s.list_price}</span>
+                        <span class="products-brand">${s.brand}</span>
+                        <span class="products-name">${s.name}</span>
+                        <span class="products-price">$${s.list_price}</span>
                     </div>
                 </div>
             `);
@@ -57,12 +44,11 @@ function loadLatest() {
 }
 
 function selectCard(productID) {
-    localStorage.setItem("viewProductId", productID); // Store the sneaker ID in local storage
+    localStorage.setItem("viewProductId", productID); 
     window.location.href = "product.html";
 }
 
 const btnScrollToTop= document.querySelector("#btnScrollToTop");
 btnScrollToTop.addEventListener("click", function(){
     window.scrollTo(0,0);
-
 });
